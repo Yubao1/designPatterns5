57083d3786ef6d9561196e5d7a18b765a6dbe557
zhouyubao1 <2535370631@qq.com>
2021-04-28 21:56:52 +0800

    4月28日第三次提交

--- designPatterns5/观察者模式/src/com/zhulu/test/Test.java
+++ designPatterns5/观察者模式/src/com/zhulu/test/Test.java
@@ -9,8 +9,8 @@ public class Test {
 	public static void main(String[] args) {
         ISend send = new Send();
         Reciever recieverA = new ReceiverA("A");
-        Reciever recieverB = new ReceiverA("B");
-        Reciever recieverC = new ReceiverA("C");
+        Reciever recieverB = new ReceiverB("B");
+        Reciever recieverC = new ReceiverC("C");
         send.register(recieverA);
         send.register(recieverC);
         send.register(recieverB);
